032595d8d012eaf48c85e462e12eae3531db202e
pascal-codecat <pascal.zwicker@windowslive.com>
2017-05-15 00:09:47 +0200

    editing xs screen

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,1 +1,28 @@
+// Navbar burger-icon
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
 
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+// Navbar burger-icon
+
+
+$(document).ready(function(){
+
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+
+});
