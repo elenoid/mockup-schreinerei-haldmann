b9b7753ca48cd36d19f088f49f6dc16dd76cded7
pascal-codecat <pascal.zwicker@windowslive.com>
2017-05-15 00:21:35 +0200

    editing xs screen

--- _site/js/main.js
+++ _site/js/main.js
@@ -9,20 +9,19 @@ function closeNav() {
 
 // Navbar burger-icon
 
-$("a").on('click', function(event) {
-  if (this.hash !== "") {
-    event.preventDefault();
-    var hash = this.hash;
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 600, function(){
-      window.location.hash = hash;
-    });
-  }
-});
 
 $(document).ready(function(){
-
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
 
 });
