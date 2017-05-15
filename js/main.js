f2b1b00d7fae069d244e922dad4817f2d4dde3bd
pascal-codecat <pascal.zwicker@windowslive.com>
2017-05-15 17:56:28 +0200

    editing xs screen

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // Navbar burger-icon
-// function openNav() {
-//     document.getElementById("mySidenav").style.width = "100%";
-// }
-//
-// function closeNav() {
-//     document.getElementById("mySidenav").style.width = "0";
-// }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
 
 // Navbar burger-icon
 
